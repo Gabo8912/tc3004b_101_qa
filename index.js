c91c7b29e26e7a4c0100673284c8fae15be7ecbf
Gabo8912 <128503197+Gabo8912@users.noreply.github.com>
2025-02-24 21:08:54 -0600

    Cambios de tarea

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 function add(){
     const num1 = document.getElementById("num1").value;
     const num2 = document.getElementById("num2").value;
-
     let result;
     result = Number(num1) + Number(num2);
 
-    document.getElementById("result").innerText= "Result: " + result;
+    document.getElementById("result").innerText = "result: " + result;
 }
 
-function sub(){
+function subtract(){
     const num1 = document.getElementById("num1").value;
     const num2 = document.getElementById("num2").value;
-
     let result;
     result = Number(num1) - Number(num2);
 
-    document.getElementById("result").innerText= "Result: " + result;
+    document.getElementById("result").innerText = "result: " + result;
 }
 
 
 
-function reset(){
-
-    document.getElementById("num1").value =" ";
-    document.getElementById("num2").value = " ";
-    document.getElementById("result").innerText="Result: "
-
+function resetFields(){
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("operator").value = "+";
+    document.getElementById("result").value = "result: ";
 }
